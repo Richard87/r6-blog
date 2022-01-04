7ab8ae0e11a3bd7b70d30a2cdfa91f0eeabb0163
Richard Hagen <richard.hagen@gmail.com>
2022-01-04 12:28:47 +0100

    init commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
-}
+})
